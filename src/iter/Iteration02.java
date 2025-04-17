f735bf9ab2a4b077f1238728e7002f8b2be8c5a2
song120437 <song120437@naver.com>
2025-04-17 16:52:32 +0900

    반복문 (for, while)

---
+++ src/iter/Iteration02.java
@@ -0,0 +1,52 @@
+package iter;
+
+public class Iteration02 {
+
+	public static void main(String[] args) {
+		// 반복문 while
+
+		/*
+
+		 while(조건식) {
+		 실행문
+		 }
+
+		 */
+		
+		//i: 1 2 3 4 5
+		for(int i=1; i<=5; i++) {
+			System.out.println("피곤하다~");
+		}
+
+		int i=1; //초기화
+		while(i<=5) { //조건식
+			//i: 1 2 3 4 5
+			
+			System.out.println("졸리다~"); //실행문
+
+			i++; //증강식
+			
+			// i=1*2
+			// i=i 50;
+		}
+
+		System.out.println("===========");
+		
+		//i:6
+		
+		//1 ~ 5 출력
+		i = 1;
+		while(i<=5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		i = 0;
+		while(i<5) {
+			i++;
+			System.out.println(i);
+			
+		}
+	}
+
+}
