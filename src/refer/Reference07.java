8b160cf8d3a36f7a744da71335aedfb53ee17345
song120437 <song120437@naver.com>
2025-04-28 12:24:09 +0900

    참조변수 배열 복사, 항상된 for문, enum

---
+++ src/refer/Reference07.java
@@ -0,0 +1,24 @@
+package refer;
+
+import java.util.Scanner;
+
+public class Reference07 {
+
+	public static void main(String[] args) {
+	
+		String day1 = "MONDAY";
+		String day2 = "FRIDAY";
+		
+		String today = "FRIDAY"; //사용자가 입력한 값
+		
+		if(today.equals(day1)) {
+			System.out.println("출근");
+		}
+		
+		if(today.equals(day2)) {
+			System.out.println("퇴근");
+	}
+
+}
+	
+}
