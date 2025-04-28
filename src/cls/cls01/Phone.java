e16e42a8a26da02c92a8b79986b14798653eafab
song120437 <song120437@naver.com>
2025-04-28 16:57:11 +0900

    클래스 시작, 필드 && 생성자

---
+++ src/cls/cls01/Phone.java
@@ -0,0 +1,5 @@
+package cls.cls01;
+
+public class Phone {
+
+}
---
+++ src/cls/cls01/PhoneMain.java
@@ -0,0 +1,18 @@
+package cls.cls01;
+
+public class PhoneMain {
+
+	public static void main(String[] args) {
+	
+		//Phone p1 = new Phone(); //
+		Phone p1 = new Phone("갤럭시21");
+
+		Phone p2 = new Phone("아이폰14");
+		
+		Phone p3 = new Phone("아이폰15",300);
+		
+		Phone p4 = new Phone("갤럭시25",250, 5000);
+
+	}
+
+}
