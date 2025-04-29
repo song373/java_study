cce6a030663975c9f7270c8f981c61e498f0742d
song120437 <song120437@naver.com>
2025-04-29 16:43:22 +0900

    연습28 클래스 기초

---
+++ src/practice28/Monitor.java
@@ -0,0 +1,21 @@
+package practice28;
+
+public class Monitor {
+     //필드
+	int inch;
+	String company;
+	String model;
+	
+	//생성자
+	Monitor(int inch, String company, String model) {
+		this.inch =inch;
+		this.company = company;
+		this.model = model;
+	}
+	
+	//메소드
+	void printinfo(){
+    System.out.printf("제조사:%s 모델명:%s 인치:%d인치\n", company, model, inch);
+		
+	}
+}
