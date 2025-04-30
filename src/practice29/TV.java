1650ae5a725f2a20ab89fe2fdc16484a23f6c1af
song120437 <song120437@naver.com>
2025-04-30 13:54:37 +0900

    연습29클래스

---
+++ src/practice29/TV.java
@@ -0,0 +1,24 @@
+package practice29;
+
+public class TV {
+
+	//필드
+	String company;
+	int year;
+	int inch;
+	
+	//생성자
+	TV(String company, int year, int inch) {
+		this.company = company;
+		this.year = year;
+		this.inch = inch;
+		
+	}
+	
+	//메소드
+	void show() {
+		System.out.printf("%s제품 %d년형 %인치 Tv\n", company, year , inch);
+		
+		
+	}
+}
