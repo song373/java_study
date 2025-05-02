1592bce9faeee0deb43fe5a5d61e81edde2feaa8
song120437 <song120437@naver.com>
2025-05-02 17:03:52 +0900

    연습34 클래스 상호작용

--- src/practice/practice33/AcccountMain.java
+++ src/practice/practice33/AcccountMain.java
@@ -4,28 +4,35 @@ public class AcccountMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		//ATM
 		Account a1 = new Account("나", 50000);
 		long insertMoney = 30000;
-		//	long result = a1.deposit(30000);
-		//System.out.println("입금액 : " + insertMoney);
-		//System.out.println("입급 후 잔액 : " + result);
 		
+		//return balance 잔액인 케이스
+//		long result = a1.deposit(insertMoney);
+//		System.out.println("입금액 : " + insertMoney);
+//		System.out.println("입금 후 잔액 : " + result);
+		
+		//return amount 입금액 케이스
 		long result = a1.deposit(insertMoney);
 		System.out.println("입금액 : " + result);
-		System.out.println("입급 후 잔액 : " + a1.getBalance());
+		System.out.println("입금 후 잔액 : " + a1.getBalance());
 		
-		long result2 = a1.withdraw(15000);
+		long result2 = a1.withdraw(150000);
 		if(result2 == 0) {
 			System.out.println("잔액 부족으로 인출 실패");
 		}
+		
 		a1.deposit(500000);
-		System.out.println("입금 후 잔액 : " +  a1.getBalance());
+		System.out.println("입금 후 잔액 : " + a1.getBalance());
+		
+		result2 = a1.withdraw(150000);
 		if(result2 == 0) {
-			System.out.println("잔액 부족으로 인한 실패");
+			System.out.println("잔액 부족으로 인출 실패");
 		}
-		System.out.println("출금 후 잔액 : + " + a1.getBalance());
+		
+		System.out.println("출금 후 잔액 : " + a1.getBalance());
 	}
 
 }
