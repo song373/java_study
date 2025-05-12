f259458ccde14daf555712e5670fb3db11aa1482
song120437 <song120437@naver.com>
2025-05-12 14:46:28 +0900

    연습42 클래스 구조

---
+++ src/practice/practice42/EduCenter.java
@@ -0,0 +1,21 @@
+package practice.practice42;
+
+import java.util.List;
+
+public class EduCenter {
+
+	//직원목록
+	List<Staff> empList;
+	//호실(방) 목록
+	List<Room> roomList;
+
+	//List<방> roomList;
+	//List<방> roomList;
+	//List<방> roomList;
+
+}
+
+//방(Room)
+//강의실
+//휴게실
+//사무실
---
+++ src/practice/practice42/Staff.java
@@ -0,0 +1,6 @@
+package practice.practice42;
+
+public class Staff {
+	//직원
+
+}
